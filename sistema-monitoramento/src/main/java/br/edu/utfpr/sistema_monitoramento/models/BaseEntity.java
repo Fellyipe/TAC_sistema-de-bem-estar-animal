1fa02f61f2ba2674f4484a4fa84af3721885fc39
Douglas Viebrantz <douglaslvbtz@gmail.com>
2025-09-10 14:36:41 -0300

    Refatorando aviario e baseentity

--- sistema-monitoramento/src/main/java/br/edu/utfpr/sistema_monitoramento/models/BaseEntity.java
+++ sistema-monitoramento/src/main/java/br/edu/utfpr/sistema_monitoramento/models/BaseEntity.java
@@ -1,23 +1,25 @@
 package br.edu.utfpr.sistema_monitoramento.models;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import java.io.Serializable;
-import java.time.LocalDateTime;
 import lombok.Data;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 @Data
 @MappedSuperclass
-public abstract class BaseEntity implements Serializable {
+public abstract class BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
     @CreationTimestamp
     @Column(name = "created_at", nullable = false, updatable = false)
