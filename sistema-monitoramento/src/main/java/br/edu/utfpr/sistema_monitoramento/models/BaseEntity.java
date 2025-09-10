26c61a2ca6b8d68cd94b087bcdbd7ff38a627a12
Douglas Viebrantz <douglaslvbtz@gmail.com>
2025-09-10 14:19:29 -0300

    classe BaseEntity

---
+++ sistema-monitoramento/src/main/java/br/edu/utfpr/sistema_monitoramento/models/BaseEntity.java
@@ -0,0 +1,29 @@
+package br.edu.utfpr.sistema_monitoramento.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Data
+@MappedSuperclass
+public abstract class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
