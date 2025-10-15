d46dcf2811edf20b17f83274dfbf6fc34bfed88a
Fellyipe <felipebueno2004@hotmail.com>
2025-10-15 15:15:24 -0300

    Rota leituras implementada

--- api-leituras/app.js
+++ api-leituras/app.js
@@ -21,7 +21,7 @@ mongoose
   });
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var leiturasRouter = require("./routes/leituras");
 
 var app = express();
 
@@ -32,6 +32,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/leituras", leiturasRouter);
 
 module.exports = app;
--- api-leituras/middlewares/isAutenticated.js
+++ api-leituras/middlewares/isAutenticated.js
@@ -1,10 +1,7 @@
-// Middleware para interceptar a autenticação do cliente
-
 const isAutenticated = (req, res, next) => {
   const { authorization } = req.headers;
 
-  // Se tiver token
-  if (authorization && authorization === "1234") return next();
+  if (authorization && authorization === "123123") return next();
 
   return res.status(401).json({ message: "Token inválido" });
 };
