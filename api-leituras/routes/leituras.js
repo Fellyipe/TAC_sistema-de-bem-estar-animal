1d9ebaa1dac9803ceb7193e1da79643b9446d1cc
Fellyipe <felipebueno2004@hotmail.com>
2025-10-15 15:15:50 -0300

    Esqueci um arquivo

--- api-leituras/routes/leituras.js
+++ api-leituras/routes/leituras.js
@@ -4,16 +4,10 @@ const { default: mongoose } = require("mongoose");
 const isAutenticated = require("../middlewares/isAutenticated");
 var router = express.Router();
 
-/* GET ALL */
 router.get("/", isAutenticated, async function (req, res, next) {
-  // const { name = '' } = req.query ? req.query : {};
-
-  // console.log("name", name);
-
   return res.json(await Leitura.find());
 });
 
-/* GET BY ID*/
 router.get("/:id", isAutenticated, async (req, res) => {
   const { id } = req.params;
 
@@ -25,51 +19,42 @@ router.get("/:id", isAutenticated, async (req, res) => {
     return res.status(400).json({ message: "ID inválido" });
   }
 
-  const user = await Leitura.findById(id);
+  const leitura = await Leitura.findById(id);
 
-  return user
-    ? res.json(user)
-    : res.status(404).json({ message: "Leitura não encontrado." });
+  return leitura
+    ? res.json(leitura)
+    : res.status(404).json({ message: "Leitura não encontrada." });
 });
 
-/* CREATE */
 router.post("/", async (req, res) => {
   const json = req.body;
 
-  const user = new Leitura(json);
+  const leitura = new Leitura(json);
 
-  const error = user.validateSync();
+  const error = leitura.validateSync();
 
-  return error ? res.status(400).json(error) : res.json(await user.save());
-
-  // console.log(user);
-
-  // users.push(user);
-
-  // res.json({ message: "Criar pessoa" });
+  return error ? res.status(400).json(error) : res.json(await leitura.save());
 });
 
-/* UPDATE */
 router.put("/:id", isAutenticated, async (req, res) => {
   const { id } = req.params;
   const json = req.body;
 
-  const user = await Leitura.findByIdAndUpdate(id, json, { new: true });
+  const leitura = await Leitura.findByIdAndUpdate(id, json, { new: true });
 
-  return user
-    ? res.json(user)
-    : res.status(404).json({ message: "Leitura não encontrado." });
+  return leitura
+    ? res.json(leitura)
+    : res.status(404).json({ message: "Leitura não encontrada." });
 });
 
-/* DELETE */
 router.delete("/:id", isAutenticated, async (req, res) => {
   const { id } = req.params;
 
-  const user = await Leitura.findByIdAndDelete(id);
+  const leitura = await Leitura.findByIdAndDelete(id);
 
-  return user
+  return leitura
     ? res.json({ message: "Leitura deletada com sucesso." })
-    : res.status(404).json({ message: "Leitura não encontrado." });
+    : res.status(404).json({ message: "Leitura não encontrada." });
 });
 
 module.exports = router;
