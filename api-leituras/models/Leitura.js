100c95f360803e6e3ba9cc46fa1dfe48db337b39
Fellyipe <felipebueno2004@hotmail.com>
2025-10-15 14:36:55 -0300

    Entidade Leitura

--- api-leituras/models/Leitura.js
+++ api-leituras/models/Leitura.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 
 const LeituraSchema = new mongoose.Schema({
   idSensor: { type: String, require: true },
+  tipoSensor: { type: String, require: true },
+  leitura: { type: String, require: true },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model("Leitura", LeituraSchema, "leitura");
